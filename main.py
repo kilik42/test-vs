8539cdd7fa9d44b1953a39cf5bb04659beb388fd
marvin evins <marvinevins@yahoo.com>
2023-10-04 08:55:48 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#data abstraction when you need to simplify data 
+# from something more complex
+name1 = "John"
+name2 = "Jane"
+name3 = "Jim"
+name4 = "Joe"
+name5 = "Jill"
+name6 = "Jen"
+name7 = "Jenna"
+name8 = "Jenny"
+name9 = "Jerome"
+name10 = "Jerry"
+name11 = "Jess"
+name12 = "Jessica"
+list1 = [name1, name2, name3, name4, name5, name6, name7, name8, name9, name10, name11, name12]
+print(list1)
+# an example of data abstraction is when I use a list to simplify data
+# instead of having to write out the same code over and over again
+# data abstraction is when you simplify data
+# if you have a list of names, you can simplify it by using a list
